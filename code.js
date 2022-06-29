93e0a4e364c915f6ddb94eebf318425c4afc6e84
Your Name <you@example.com>
2022-06-28 19:51:14 -0600

    collect data from the input field using getElementById method

---
+++ code.js
@@ -0,0 +1,7 @@
+function getInfo(){
+    //var newName is going to collect data from the input field
+    var newName = document.getElementById("searchCity");
+    var cityName = document.getElementById("city").innerHTML=newName.value
+
+}
+//get weather details from API using javascript promise/ fetch method
