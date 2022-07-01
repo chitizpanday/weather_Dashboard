eb8a9714831b9d5d70d212a7cb85d36d13f67bdc
Your Name <you@example.com>
2022-07-01 14:43:46 -0600

    Added html and CSScontents

--- code.js
+++ code.js
@@ -6,8 +6,13 @@ function getInfo(){
 }
 //get weather details from API using javascript promise/ fetch method
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=newName&appid=e3e7f409d7554da4b1757864ff62f226")
-.then(response=>response.json())
-.than(data=>{
-    
+fetch("https://api.openweathermap.org/data/2.5/forecast?q=texas&appid=e3e7f409d7554da4b1757864ff62f226")
+// .then(response=>response.json())
+// .than(data=>{})
+
+.then(function(response){
+    return response.json();
 })
+.then(function(result){
+console.log(result);
+});
